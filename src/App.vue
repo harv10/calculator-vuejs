fbade7fbb4019713cc4440c32bdfdfbf87274cf2
harvinder singh <105470189+harv10@users.noreply.github.com>
2023-11-26 19:18:23 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <h1 style=" margin:10px;font-family:monospace">Calculator</h1>
+  <DisplayScreen :output="this.output" />
+  <KeyBoard @key-press="updateScreen" />
+  <div class="footer m-lg-3">
+    
+   <p style="margin-top:15px;">
+   Group Members: <br/>
+   Harvinder Singh <br/>
+   Mohammad Sohail Khan <br/>
+   Nevin Reby Thomas <br/>
+   Raj Karan Singh Sagoo 
+   </p></div>
+</template>
+
+<script>
+import DisplayScreen from "./components/DisplayScreen.vue";
+import KeyBoard from "./components/KeyBoard.vue";
+
+export default {
+  name: "App",
+  components: {
+    DisplayScreen,
+    KeyBoard,
+  },
+  data() {
+    return {
+      output: "0",
+      calval:'0',
+      operator: null,
+      preCalVal: "",
+    };
+  },
+  methods: {
+    updateScreen(output) {
+      /* Append Value */
+      if (!isNaN(output) || output === ".") {
+        if (this.calval === "0") {
+          console.log("c1");
+
+          this.output = output;
+          this.calval = output;
+        } else {
+          this.output += output;
+          this.calval +=output;
+
+          console.log("c2");
+        }
+      }
+      
+      /* Clear Value */
+      if (output === "C") {
+        this.output = "0";
+        this.preCalVal='0'
+        this.operator=null;
+        this.calval='0'
+      }
+      
+      /* Percentage */
+      if (output === "%") {
+        this.output = this.output / 100 + "";
+        this.calval=this.output/100+'';
+      }
+      
+      /* Append Value */
+      if(['+','-','*','/'].includes(output)){
+        this.operator = output;
+        this.preCalVal=this.calval
+        this.output=this.preCalVal+this.operator;
+        this.calval=this.preCalVal+this.operator;
+      }
+      if(output==='='){
+        this.output= eval(this.calval);
+        this.preCalVal='';
+        this.operator=null;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.social-ic{
+  margin: 8px;
+}
+.social-ic:hover{
+  cursor: pointer;
+  box-shadow: 0px 1px 0px 2px rgba(0, 0, 0, 0.259);
+}
+</style>
---
+++ src/components/KeyBoard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="key-border py-3 px-3" style="max-width:450px; margin:auto; background: #234;">
+    <div class="row no-gutters">
+      <div class="col-3"  @click="emit(n)" v-for="n in calculatorkeys" :key="n">
+        <div class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class"
+        :class="{'green-bg':['%','C','/','x','*','+','-','='].includes(n)}" >
+          {{n}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "KeyBoard",
+  props: {},
+  data(){
+    return{
+      calculatorkeys:['%','*','/','C',7,8,9,'-',4,5,6,'+',1,2,3,'=','00',0,'.'],
+      // calculatorkeys:['C','%','/','x',7,8,9,'*',4,5,6,'-',1,2,3,'+','00',0,'.','='],
+
+    }
+  },
+  methods:{
+      emit(a){
+          this.$emit('key-press',a+'');
+      }
+  }
+};
+</script>
+<style scoped>
+
+.key-border {
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  margin-top: 5px;
+}
+.row {
+  display: flex;
+}
+.bg-vue-dark {
+  background: #31475e;
+}
+
+.green-bg{
+  background: rgb(64, 109, 231);
+}
+.hover-class:hover{
+  background: #000;
+  cursor: pointer;
+}
+</style>
